1848617c9b670ae035d511bce43386eedc1ac91a
Oleksii Kovalenko <a16kova@gmail.com>
2024-07-08 08:23:51 +0000

    Create Car Model and install cloudinary

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Car(models.Model):
+    """
+    Car model to be able to create many Cars in Django's admin panel
+    """
+
+    TYPE_CHOICES = [
+        ('Sedan', 'Sedan'),
+        ('Coupe', 'Coupe'),
+        ('Cabriolet', 'Cabriolet'),
+        ('Hatchback', 'Hatchback'),
+        ('Station Wagon', 'Station Wagon'),
+        ('Minivan', 'Minivan'),
+        ('SUV', 'SUV'),
+        ('Pickup Truck', 'Pickup Truck'),
+    ]
+    FUEL_CHOICES = [
+        ('Petrol', 'Petrol'),
+        ('Diesel', 'Diesel'),
+        ('Electric', 'Electric')
+    ]
+    TRANSMISSION_CHOICES = [
+        ('MT', 'Manual Transmission'),
+        ('AT', 'Automatic Transmission'),
+    ]
+
+    make = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    type = models.CharField(
+        max_length=15, choices=TYPE_CHOICES
+    )
+    year = models.IntegerField()
+    seats = models.IntegerField()
+    doors = models.IntegerField()
+    fuel_type = models.CharField(
+        max_length=8, choices=FUEL_CHOICES, default="petrol"
+    )
+    transmission = models.CharField(max_length=2, choices=TRANSMISSION_CHOICES)
+    air_conditioning = models.BooleanField()
+    navigation = models.BooleanField()
+    availability = models.BooleanField(default=True)
+    price_per_day = models.DecimalField(max_digits=10, decimal_places=2)
+    image = CloudinaryField('image', default='placeholder')
+    
