dc3aa857639295a6c5d629adbb0ab716a14aaa10
Oleksii Kovalenko <a16kova@gmail.com>
2024-07-09 09:33:15 +0000

    Install allauth and add code regarding authentication to header in base.html

--- car_rental/urls.py
+++ car_rental/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
     path("locations", include("offices.urls"), name="office-urls"),
     path('summernote/', include('django_summernote.urls')),
