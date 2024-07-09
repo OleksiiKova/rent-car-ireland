5c3432cac8e79625ca8e359b9cd7a3342927c9f9
Oleksii Kovalenko <a16kova@gmail.com>
2024-07-09 17:55:55 +0000

    Add map to location page

--- car_rental/settings.py
+++ car_rental/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-oleksiikova-rentcarirel-8ngrzpl5wn4.ws-eu115.gitpod.io',
+ALLOWED_HOSTS = ['8000-oleksiikova-rentcarirel-fckfnxl98y0.ws.codeinstitute-ide.net',
                 '.herokuapp.com']
 
 
