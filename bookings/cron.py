519092644599382ddf3a19a48aea3d251c2668d6
Oleksii Kovalenko <a16kova@gmail.com>
2024-07-20 13:46:32 +0000

    Change UpdateBookingStatusCronJob(CronJobBase): (delete do:)

--- bookings/cron.py
+++ bookings/cron.py
@@ -2,15 +2,12 @@ from django_cron import CronJobBase, Schedule
 from .models import Booking
 from django.utils import timezone
 
-class UpdateBookingStatusCronJob(CronJobBase):
-    RUN_AT_TIMES = ['00:00']
 
-    schedule = Schedule(run_at_times=RUN_AT_TIMES)
+class UpdateBookingStatusCronJob(CronJobBase):
     code = 'bookings.update_booking_status'
 
-    def do(self):
-        today = timezone.now().date()
-        bookings = Booking.objects.filter(end_date__lt=today, status='confirmed')
-        for booking in bookings:
-            booking.status = 'completed'
-            booking.save()
+    today = timezone.now().date()
+    bookings = Booking.objects.filter(end_date__lt=today, status='confirmed')
+    for booking in bookings:
+        booking.status = 'completed'
+        booking.save()
