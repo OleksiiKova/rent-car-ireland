00c9ca707e40c12f02d8da193620b63fb65b6370
Oleksii Kovalenko <a16kova@gmail.com>
2024-08-17 13:12:35 +0000

    Add summernote to the Admin Car

--- bookings/admin.py
+++ bookings/admin.py
@@ -2,16 +2,27 @@ from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
 from .models import Booking
 
+
 # Register your models here.
 @admin.register(Booking)
 class BookingAdmin(SummernoteModelAdmin):
     """
-    Admin configuration for the Booking model.
+    Admin interface configuration for the Booking model.
 
-    This class customizes the admin interface for the Booking model,
-    specifying which fields to display in the list view and other
-    administrative options.
+    This class customizes the Django admin interface for the Booking model
+    using Summernote for rich text editing. It specifies which fields to
+    display in the list view, enables search functionality for the user field,
+    and allows filtering by car and creation date.
+
+    Attributes:
+        list_display (tuple): Fields to display in the list view, including
+                              the user, car, start date, end date, and
+                              creation date.
+        search_fields (list): Fields that can be searched, specifically the
+                              user.
+        list_filter (tuple): Fields by which the list can be filtered,
+                              including car and creation date.
     """
-    list_display = ('user', 'start_date', 'end_date')
-    search_fields = ['car']
-    list_filter = ('car',)
+    list_display = ('user', 'car', 'start_date', 'end_date', 'created_at')
+    search_fields = ['user']
+    list_filter = ('car', 'created_at')
--- cars/admin.py
+++ cars/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
 from .models import Car
 
+
 # Register your models here.
-admin.site.register(Car)
+@admin.site.register(Car)
+class CarAdmin(SummernoteModelAdmin):
+    """
+    Admin interface configuration for the Car model.
+
+    This class customizes the Django admin interface for the Car model using
+    Summernote for rich text editing. It specifies which fields to display in
+    the list view, enables search functionality on specific fields, and allows
+    filtering of the list by various attributes of the car.
+
+    Attributes:
+        list_display (tuple): Fields to display in the list view, including
+                              make, model, type, transmission, and fuel type.
+        search_fields (list): Fields that can be searched, specifically
+                              make and model.
+        list_filter (tuple): Fields by which the list can be filtered,
+                              including make, model, type, transmission,
+                              and fuel type.
+    """
+    list_display = ('make', 'model', 'type', 'transmission', 'fuel_type')
+    search_fields = ['make', 'model']
+    list_filter = ('make', 'model', 'type', 'transmission', 'fuel_type')
