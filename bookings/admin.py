5ab5e6009d1c0eedc47e5ebd55ea7a39fb7edefe
Oleksii Kovalenko <a16kova@gmail.com>
2024-08-17 14:11:47 +0000

    Add 'car__make', 'car__model' to the seasrh field to the Booking admin

--- bookings/admin.py
+++ bookings/admin.py
@@ -23,7 +23,13 @@ class BookingAdmin(SummernoteModelAdmin):
         list_filter (tuple): Fields by which the list can be filtered,
                               including car and creation date.
     """
-    list_display = ('first_name', 'last_name', 'car', 'start_date',
+    list_display = ('full_name', 'car', 'start_date',
                     'end_date', 'created_at')
-    search_fields = ['first_name', 'last_name', 'car']
+    search_fields = ['first_name', 'last_name', 'car__make', 'car__model']
     list_filter = ('car', 'created_at')
+
+    def full_name(self, obj):
+        """Return the full name by combining first and last name."""
+        return f"{obj.first_name} {obj.last_name}"
+    
+    full_name.short_description = 'Full Name'
